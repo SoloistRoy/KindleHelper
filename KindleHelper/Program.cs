3b1f83b18bebaf687e93c1492a3b303f86df9143
Engineer <yh20021212@126.com>
2017-09-18 07:54:20 +0800

    No message

--- KindleHelper/Program.cs
+++ KindleHelper/Program.cs
@@ -17,6 +17,7 @@ namespace KindleHelper
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormSearch());
+            //
         }
     }
 }
